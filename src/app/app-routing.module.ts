cd3fb407938a62265f0c76620a23522c6db33749
AlEz-D27 <122579183+AlEz-D27@users.noreply.github.com>
2023-10-23 00:19:03 -0300

    Login Agregado

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: '', component: LoginComponent }, // Set the root path to the LoginComponent
+  { path: 'app', component: AppComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
